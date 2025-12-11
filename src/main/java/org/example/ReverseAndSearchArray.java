a092be01442faf73aaaa195d1683bb2e024cab55
Recep Borekci <recep.borekci@qaracter.com>
2025-12-11 15:40:09 +0300

    Implemented the remaining features, reverse the array, print the array, and search with index output.

--- src/main/java/org/example/ReverseAndSearchArray.java
+++ src/main/java/org/example/ReverseAndSearchArray.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ReverseAndSearchArray {
@@ -15,6 +17,17 @@ public class ReverseAndSearchArray {
 
         System.out.println("The numbers you entered are: ");
         printArray(inputArray, inputNumber);
+
+        System.out.println();
+
+        int[] reversedArray = reverseArray(inputArray);
+        System.out.println("Reversed array: " + Arrays.toString(reversedArray));
+
+        System.out.println("Enter a number to search: ");
+        int searchNumber = readInt(scanner);
+        searchNumberInArray(inputNumber, inputArray, searchNumber);
+
+
     }
 
     public static int readInt(Scanner sc) {
@@ -38,5 +51,33 @@ public class ReverseAndSearchArray {
         for (int i = 0; i < arrayLength; i++) {
             System.out.print(array[i] + " ");
         }
+    }
+
+    public static int[] reverseArray(int[] array) {
+        int temp;
+        for (int i = 0; i < array.length / 2; i++) {
+            temp = array[i];
+            array[i] = array[array.length - i - 1];
+        }
+        return array;
+    }
+
+    private static void searchNumberInArray(int inputNumber, int[] inputArray, int searchNumber) {
+        ArrayList<Integer> indices = new ArrayList<Integer>();
+        boolean found = false;
+
+        for (int i = 0; i < inputNumber; i++) {
+            if (inputArray[i] == searchNumber) {
+                found = true;
+                indices.add(i);
+            }
+        }
+
+        if (found) {
+            System.out.println("Your number is in the array. At the indices: " + indices);
+        } else {
+            System.out.println("Your number is not in the array.");
+        }
+
     }
 }
